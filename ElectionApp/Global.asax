768874e9c49b83ed0720b245d7a5defca7fa3c4e
maximugisha@gmail.com <maximugisha@gmail.com>
2017-08-04 11:06:10 +0300

    designing the ballot page and tally oppage

---
+++ ElectionApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElectionApp.MvcApplication" Language="C#" %>
---
+++ ElectionApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElectionApp.Startup))]
+namespace ElectionApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
