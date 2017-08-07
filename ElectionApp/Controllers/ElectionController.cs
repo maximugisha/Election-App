6569a7ebebd760189ab13c546964465272d13190
maximugisha@gmail.com <maximugisha@gmail.com>
2017-08-07 14:59:06 +0300

    aading the ballot functionality

--- ElectionApp/Controllers/ElectionController.cs
+++ ElectionApp/Controllers/ElectionController.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace ElectionApp.Controllers
 {
     public class ElectionController : Controller
     {
         // GET: Election
+        MySqlConnection connection = new MySqlConnection("server=localhost; user = Maximo; database = election_db; port = 3306; password = 'UPbeat123'; ");
         public ActionResult Index()
         {
             return View();
@@ -16,7 +20,15 @@ namespace ElectionApp.Controllers
         public ActionResult ballotpage()
         {
             ViewBag.Message = "Cast Your Vote Here.";
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM `candidates`", connection);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable("candidates");
+            da.Fill(dt);
+            //dbtable.ItemsSource = dt.DefaultView;
 
+            connection.Close();
+            
             return View();
         }
         public ActionResult tallypage()
