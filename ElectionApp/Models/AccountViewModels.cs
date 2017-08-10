da6f59b6b67002c2d3fa656fb00f4618774d4c28
elizabeth kamayangi <kamayangie@gmail.com>
2017-08-10 11:47:55 -0700

    added more fields in the reg page

--- ElectionApp/Models/AccountViewModels.cs
+++ ElectionApp/Models/AccountViewModels.cs
@@ -54,6 +54,26 @@ namespace ElectionApp.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Registration Number")]
+        public string RegistrationNumber { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
