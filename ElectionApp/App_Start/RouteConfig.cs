0a7f69b660e4b95a9ac1a4b94a43c0bfae05801f
maximugisha <maximugisha@gmail.com>
2017-08-16 13:01:34 +0300

    unmerge

--- ElectionApp/App_Start/RouteConfig.cs
+++ ElectionApp/App_Start/RouteConfig.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,34 +27,3 @@ namespace ElectionApp
 
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-
-namespace ElectionApp
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-            routes.MapRoute(
-                name: "Login",
-                url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Election", action = "ballotpage", id = UrlParameter.Optional }
-           );
-        }
-
-    }
-}
->>>>>>> 083db1f586db87e015e0c4b5d9bce3f38de76c23
--- ElectionApp/Scripts/_references.js
+++ ElectionApp/Scripts/_references.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿/// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
 /// <reference path="jquery-3.1.1.js" />
@@ -7,16 +6,3 @@
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="respond.js" />
-=======
-﻿/// <autosync enabled="true" />
-/// <reference path="jquery-3.1.1.js" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="bootstrap.js" />
-/// <reference path="respond.js" />
-/// <reference path="jquery-3.1.1.slim.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="../test.js" />
-/// <reference path="chart.js" />
-/// <reference path="getorgchart.js" />
->>>>>>> 083db1f586db87e015e0c4b5d9bce3f38de76c23
